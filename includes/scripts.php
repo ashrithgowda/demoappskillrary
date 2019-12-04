9ab9e5cff898755bffd1f7f87aaf1882342a40a3
Deepak <deepak.lohani@skillrary.com>
2019-12-04 16:12:32 +0530

    javascript confirm box added

--- includes/scripts.php
+++ includes/scripts.php
@@ -43,24 +43,47 @@ $(function(){
 
   $('#productForm').submit(function(e){
   	e.preventDefault();
-  	var product = $(this).serialize();
-  	$.ajax({
-  		type: 'POST',
-  		url: 'cart_add.php',
-  		data: product,
-  		dataType: 'json',
-  		success: function(response){
-  			$('#callout').show();
-  			$('.message').html(response.message);
-  			if(response.error){
-  				$('#callout').removeClass('callout-success').addClass('callout-danger');
-  			}
-  			else{
-				$('#callout').removeClass('callout-danger').addClass('callout-success');
-				getCart();
-  			}
-  		}
-  	});
+
+      if (confirm("Are you sure you want to add to cart ?")) {
+          var product = $(this).serialize();
+          $.ajax({
+            type: 'POST',
+            url: 'cart_add.php',
+            data: product,
+            dataType: 'json',
+            success: function(response){
+              $('#callout').show();
+              $('.message').html(response.message);
+              if(response.error){
+                $('#callout').removeClass('callout-success').addClass('callout-danger');
+              }
+              else{
+              $('#callout').removeClass('callout-danger').addClass('callout-success');
+              getCart();
+              }
+            }
+          });
+      }
+  	// var product = $(this).serialize();
+  	// $.ajax({
+  	// 	type: 'POST',
+  	// 	url: 'cart_add.php',
+  	// 	data: product,
+  	// 	dataType: 'json',
+  	// 	success: function(response){
+  	// 		$('#callout').show();
+  	// 		$('.message').html(response.message);
+  	// 		if(response.error){
+  	// 			$('#callout').removeClass('callout-success').addClass('callout-danger');
+  	// 		}
+  	// 		else{
+			// 	$('#callout').removeClass('callout-danger').addClass('callout-success');
+			// 	getCart();
+  	// 		}
+  	// 	}
+  	// });
+
+
   });
 
   $(document).on('click', '.close', function(){
